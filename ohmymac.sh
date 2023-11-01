b9340ec8942241311564672e61d89b3189e668c7
Lev Rogozhin <lev@rogozhin.pro>
2023-11-01 16:40:40 +0100

    move preparations into separate script

--- ohmymac.sh
+++ ohmymac.sh
@@ -2,36 +2,23 @@
 
 source ./scripts/interact.sh
 source ./scripts/env.sh
+source ./scripts/prepare.sh
 
-installBrew(){
-  title "Brew"
+installBrewDevApps(){
+  printAction "Install wget"
+  brew install wget
+  printAction "Install mc"
+  brew install mc
+  printAction "Install redis"
+  brew install redis
+  brew services start redis
 
-  if [ -z "$(brew --version 2>/dev/null)" ]
-  then
-    printAction "Install brew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
-  fi
-
-  printAction "Install mas"
-  brew install mas
-
-  if [ $DO_DEV_APPS -eq 1 ]; then
-    printAction "Install wget"
-    brew install wget
-    printAction "Install mc"
-    brew install mc
-    printAction "Install redis"
-    brew install redis
-    brew services start redis
-
-    printAction "Install postgres"
-    brew install postgres
-    brew services start postgres
-    mkdir -p ~/Library/LaunchAgents
-    ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-    launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-  fi
+  printAction "Install postgres"
+  brew install postgres
+  brew services start postgres
+  mkdir -p ~/Library/LaunchAgents
+  ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 }
 
 installApp(){
@@ -74,48 +61,6 @@ installApp(){
   rm -rf ./tmp
 }
 
-doInitiatory(){
-  if [[ "${OS}" != "Darwin" ]]
-  then
-  	die "Oh! My not a Mac!"
-  fi
-
-  if [ -z "${BASH_VERSION:-}" ]
-  then
-  	die "Bash is required to interpret this script"
-  fi
-
-  if [ -z "$(wget -V 2>/dev/null)" ] && [ $DO_MACOS_PREFS -eq 0 ]
-  then
-    if [ $DO_DEV_APPS -eq 1 ] || [ $DO_DEV_APPS -eq 1 ]
-    then
-      die "wget is required to download applications"
-    fi
-  fi
-
-
-  if [ -z "$(xcode-select -p 2>/dev/null)" ]
-  then
-  	echo "${txtred}CLT for Xcode or Xcode is required${txtrst}"
-  	echo "Do 'xcode-select --install' or install Xcode"
-  	exit 1
-  fi
-
-  if [ -z "$(groups | grep admin)" ]
-  then
-  	HAS_SUDO_RIGHTS=0
-  fi
-
-  # Close any open System Preferences panes, to prevent them from overriding settings
-  osascript -e 'tell application "System Preferences" to quit'
-
-  if [ $HAS_SUDO_RIGHTS -eq 0 ] && [ $NEED_SUDO -eq 1 ]
-  then
-  	# Ask password and prevent to ask again
-  	sudo -v
-  	while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-  fi
-}
 doFinish(){
   waitForKey "press any key to reboot"
   osascript -e 'tell application "System Events" to restart'
@@ -401,11 +346,8 @@ then
   waitForKey "Be sure to log into the AppStore then press any key to start"
 fi
 
-doInitiatory
-
 if [ $DO_MACOS_PREFS -eq 1 ]; then
   doMacPreferences
-  installBrew
 fi
 
 if [ $DO_ALL -eq 1 ]; then
@@ -413,6 +355,7 @@ if [ $DO_ALL -eq 1 ]; then
 fi
 
 if [ $DO_DEV_APPS -eq 1 ]; then
+  installBrewDevApps
   doDevApps
 fi
 
---
+++ scripts/prepare.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+checkConditions(){
+  if [[ "${OS}" != "Darwin" ]]
+  then
+  	die "Oh! My not a Mac!"
+  fi
+
+  if [ -z "${BASH_VERSION:-}" ]
+  then
+  	die "Bash is required to interpret this script"
+  fi
+
+  if [ -z "$(wget -V 2>/dev/null)" ] && [ $DO_MACOS_PREFS -eq 0 ]
+  then
+    if [ $DO_DEV_APPS -eq 1 ] || [ $DO_DEV_APPS -eq 1 ]
+    then
+      die "wget is required to download applications"
+    fi
+  fi
+
+
+  if [ -z "$(xcode-select -p 2>/dev/null)" ]
+  then
+  	echo "${txtred}CLT for Xcode or Xcode is required${txtrst}"
+  	echo "Do 'xcode-select --install' or install Xcode"
+  	exit 1
+  fi
+}
+
+installBrew(){
+  if [ -z "$(brew --version 2>/dev/null)" ]
+  then
+    printAction "Install brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
+  fi
+}
+
+installMas(){
+  if [ -z "$(mas version 2>/dev/null)" ]
+  then
+    printAction "Install mas"
+    brew install mas
+  fi
+}
+
+init(){
+  if [ -z "$(groups | grep admin)" ]
+  then
+  	HAS_SUDO_RIGHTS=0
+  fi
+
+  # Close any open System Preferences panes, to prevent them from overriding settings
+  osascript -e 'tell application "System Preferences" to quit'
+
+  if [ $HAS_SUDO_RIGHTS -eq 0 ] && [ $NEED_SUDO -eq 1 ]
+  then
+  	# Ask password and prevent to ask again
+  	sudo -v
+  	while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+  fi
+
+  title "Prepare environment"
+
+  # TODO install if need
+  installBrew
+  installMas
+}
+
+checkConditions
+init
